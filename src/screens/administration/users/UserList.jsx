ab4d0eefd1e685e91a191f39133b8ab8464444f3
anthonyndungu <anthonyndungu069@gmail.com>
2024-03-08 16:01:19 +0300

    Added users report..

--- src/screens/administration/users/UserList.jsx
+++ src/screens/administration/users/UserList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 //import { useGetTodosQuery } from './apiSlice';
 import Loader from "../../../components/Loader";
 import { useGetUsersQuery } from "../../../slices/administration/usersApiSlice";
@@ -8,47 +8,142 @@ import { FaRegFileExcel } from "react-icons/fa6";
 import { CiEdit } from "react-icons/ci";
 import { BsFileEarmarkPdf } from "react-icons/bs";
 import { IoMdEye } from "react-icons/io";
+import { FaFilePdf, FaFileExcel } from "react-icons/fa";
+import DataTable from "../../../components/general/DataTable";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const UserList = () => {
-  const { data, isLoading } = useGetUsersQuery();
+  const { data: users, isLoading } = useGetUsersQuery();
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    if (users?.data) {
+      setTableData(users.data);
+    }
+  }, [users]);
+
+  const handleDownloadPDF = async () => {
+    try {
+      await axios({
+        url: "http://localhost:3000/api/v1/reports/all/users/pdf", // Endpoint on your Node.js server
+        method: "GET",
+        responseType: "blob", // Important: responseType 'blob' for binary data
+      }).then((response) => {
+        // Create a blob object from the binary data
+        const blob = new Blob([response.data], { type: "application/pdf" });
+
+        // Create a link element to trigger the download
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "all-users-report.pdf");
+        document.body.appendChild(link);
+
+        // Trigger the download
+        link.click();
+
+        // Clean up
+        window.URL.revokeObjectURL(url);
+      });
+    } catch (error) {
+      console.log(error.message);
+      // toast.error(error.message);
+    }
+  };
+  const handleDownloadExcel = async () => {
+    try {
+      await axios({
+        url: "http://localhost:3000/api/v1/reports/all/users/excel", // Endpoint on your Node.js server
+        method: "GET",
+        responseType: "blob", // Important: responseType 'blob' for binary data
+      }).then((response) => {
+        // Create a blob object from the binary data
+        const blob = new Blob([response.data], {
+          type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        });
+
+        // Create a link element to trigger the download
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "all-users-report.xlsx");
+        document.body.appendChild(link);
+
+        // Trigger the download
+        link.click();
+
+        // Clean up
+        window.URL.revokeObjectURL(url);
+      });
+    } catch (error) {
+      console.log(error.message);
+      // toast.error(error.message);
+    }
+  };
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "#",
+        accessor: (row, index) => index + 1,
+      },
+      {
+        Header: "First Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last Name",
+        accessor: "last_name",
+      },
+      {
+        Header: "Email",
+        accessor: "user_email",
+      },
+      {
+        Header: "Edit",
+        accessor: "edit",
+        Cell: ({ row }) => (
+          <Link to="#">
+            <CiEdit />
+          </Link>
+        ),
+      },
+      {
+        Header: "View",
+        accessor: "view",
+        Cell: ({ row }) => (
+          <Link to="#">
+            <IoMdEye />
+          </Link>
+        ),
+      },
+    ],
+    []
+  );
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <>
-      <Table striped style={{ border: "1px solid #ccc" }}>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>EMAIL</th>
-            <th>Edit</th>
-            <th>View</th>
-          </tr>
-        </thead>
-        <tbody>
-          {isLoading ? (
-            <Loader />
-          ) : (
-            data?.data.map((user, index) => (
-              <tr>
-                <td>{index + 1}</td>
-                <td>{user.first_name}</td>
-                <td>{user.last_name}</td>
-                <td>{user.user_email}</td>
-                <td>
-                  <Link to="#">
-                    <CiEdit />
-                  </Link>
-                </td>
-                <td>
-                  <Link to="#">
-                    <IoMdEye />
-                  </Link>
-                </td>
-              </tr>
-            ))
-          )}
-        </tbody>
-      </Table>
+      <div>
+        <p>*** All Staffs ***</p>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <div style={{ marginLeft: "10px" }}>
+            <button onClick={handleDownloadPDF}>
+              <FaFilePdf />
+            </button>
+          </div>
+          <div style={{ marginLeft: "10px" }}>
+            <button onClick={handleDownloadExcel}>
+              <FaFileExcel />
+            </button>
+          </div>
+        </div>
+        <DataTable columns={columns} data={tableData} />
+      </div>
     </>
   );
 };
