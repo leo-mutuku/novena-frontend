b63e06534c03b8e8c81626602f5ebc708463705f
mogulafric <info@mogulafric.com>
2024-03-09 09:33:57 +0300

    Added Updated Production and Accounts modules...

--- src/screens/production/packhousepeople/PackhosePeopleList.jsx
+++ src/screens/production/packhousepeople/PackhosePeopleList.jsx
@@ -1,63 +1,145 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 //import { useGetTodosQuery } from './apiSlice';
 import Loader from "../../../components/Loader";
 import { useGetAllPackHousePeopleQuery } from "../../../slices/production/packHousePeopleApiSlice";
 import { Table, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { FaRegFileExcel } from "react-icons/fa6";
 import { CiEdit } from "react-icons/ci";
 import { BsFileEarmarkPdf } from "react-icons/bs";
 import { IoMdEye } from "react-icons/io";
+import DataTable from "../../../components/general/DataTable";
+import axios from "axios";
+import { baseUrlJasper } from "../../../slices/baseURLJasperReports";
+import { FaRegFileExcel, FaFilePdf, FaFileExcel } from "react-icons/fa";
 
 const PackhosePeopleList = () => {
-  const { data, isLoading } = useGetAllPackHousePeopleQuery();
+  const { data: persons, isLoading } = useGetAllPackHousePeopleQuery();
+  const [tableData, setTableData] = useState([]);
+  const [loadingPdf, setLoadingPdf] = useState(false);
+  const [loadingExcel, setLoadingExcel] = useState(false);
+
+  useEffect(() => {
+    if (persons?.data) {
+      setTableData(persons.data);
+    }
+  }, [persons]);
+
+  const handleDownloadPDF = async () => {
+    setLoadingPdf(true);
+    try {
+      const response = await axios({
+        url: `${baseUrlJasper}/all/packhousepple/pdf`, // Endpoint on your Node.js server
+        method: "GET",
+        responseType: "blob", // Important: responseType 'blob' for binary data
+      });
+      const blob = new Blob([response.data], { type: "application/pdf" });
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "all-packhousepple-report.pdf");
+      document.body.appendChild(link);
+      link.click();
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading PDF:", error);
+    } finally {
+      setLoadingPdf(false);
+    }
+  };
+
+  const handleDownloadExcel = async () => {
+    setLoadingExcel(true);
+    try {
+      const response = await axios({
+        url: `${baseUrlJasper}/all/packhousepple/excel`, // Endpoint on your Node.js server
+        method: "GET",
+        responseType: "blob", // Important: responseType 'blob' for binary data
+      });
+      const blob = new Blob([response.data], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      });
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "all-packhousepple-report.xlsx");
+      document.body.appendChild(link);
+      link.click();
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading Excel:", error);
+    } finally {
+      setLoadingExcel(false);
+    }
+  };
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "#",
+        accessor: (row, index) => index + 1,
+      },
+      {
+        Header: "Pack.h no",
+        accessor: "packhouse_person_number",
+      },
+      {
+        Header: "First name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last name",
+        accessor: "last_name",
+      },
+      {
+        Header: "Email",
+        accessor: "email",
+      },
+      {
+        Header: "Phone",
+        accessor: "phone",
+      },
+      {
+        Header: "Edit",
+        accessor: "edit",
+        Cell: ({ row }) => (
+          <Link
+            to={`/production/packhousepeople/deletepackhouseperson/${row.original.staff_id}`}
+          >
+            <CiEdit />
+          </Link>
+        ),
+      },
+      {
+        Header: "View",
+        accessor: "view",
+        Cell: () => (
+          <Link to="#">
+            <IoMdEye />
+          </Link>
+        ),
+      },
+    ],
+    []
+  );
 
   return (
     <>
-      <p>*** All packhouse people ***</p>
-      <Table striped style={{ border: "1px solid #ccc" }}>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Pack.h no</th>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Email</th>
-            <th>phone</th>
-            <th>Edit</th>
-            <th>View</th>
-          </tr>
-        </thead>
-        <tbody>
-          {isLoading ? (
-            <Loader />
-          ) : (
-            data?.data?.map((item, index) => (
-              <tr>
-                <td>{index + 1}</td>
-                <td>{item.packhouse_person_number}</td>
-                <td>{item.first_name}</td>
-                <td>{item.last_name}</td>
-                <td>{item.email}</td>
-                <td>{item.phone}</td>
-
-                <td>
-                  <Link
-                    to={`/production/packhousepeople/deletepackhouseperson/${item.staff_id}`}
-                  >
-                    <CiEdit />
-                  </Link>
-                </td>
-                <td>
-                  <Link to="#">
-                    <IoMdEye />
-                  </Link>
-                </td>
-              </tr>
-            ))
-          )}
-        </tbody>
-      </Table>
+      <div>
+        <p>*** All Pack House people ***</p>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <div style={{ marginLeft: "10px" }}>
+            <button onClick={handleDownloadPDF} disabled={loadingPdf}>
+              {loadingPdf ? <Loader /> : <FaFilePdf />}
+            </button>
+          </div>
+          <div style={{ marginLeft: "10px" }}>
+            <button onClick={handleDownloadExcel} disabled={loadingExcel}>
+              {loadingExcel ? <Loader /> : <FaFileExcel />}
+            </button>
+          </div>
+        </div>
+        <DataTable columns={columns} data={tableData} />
+      </div>
     </>
   );
 };
