d7f187f8a3e43f682e41225804815f34aa241b72
anthonyndungu <anthonyndungu069@gmail.com>
2024-04-02 10:53:22 +0300

    Finishing up react tables...

--- src/screens/sales/orders/OrderHeaderListTest.jsx
+++ src/screens/sales/orders/OrderHeaderListTest.jsx
@@ -1,173 +1,3 @@
-// import React, { useEffect, useMemo, useState } from "react";
-// import Loader from "../../../components/Loader";
-// import { useGetAllSalesOrderHeadersQuery } from "../../../slices/sales/salesOrderHeadersApiSlice";
-// import { Table, Button } from "react-bootstrap";
-// import { Link, useNavigate } from "react-router-dom";
-// import { FaRegFileExcel } from "react-icons/fa6";
-// import { CiEdit } from "react-icons/ci";
-// import { BsFileEarmarkPdf } from "react-icons/bs";
-// import { IoMdAdd } from "react-icons/io";
-// import { MdDelete } from "react-icons/md";
-// import AddOrderLines from "./lines/AddOrderLines";
-// // import DeletePurchaseModal from "./lines/DeletePurchaseModal";
-// import TimeDate from "../../../components/TimeDate";
-// import PrintSalesOrder from "./lines/PrintSalesOrder";
-// import { TiPrinter } from "react-icons/ti";
-// import DataTable from "../../../components/general/DataTable";
-// import moment from "moment";
-
-// const OrderHeaderList = () => {
-//   const { data: orders, isLoading } = useGetAllSalesOrderHeadersQuery();
-//   const [tableData, setTableData] = useState([]);
-//   let timeDate = new TimeDate();
-//   const [mode_print, set_mode_print] = useState("none");
-//   const [mode, set_mode] = useState("none");
-//   const [mode_delete, set_mode_delete] = useState("none");
-//   const [store_purchase_id, set_store_purchase_id] = useState("");
-
-//   const handleAdd = (e, id, style) => {
-//     set_store_purchase_id(parseInt(id));
-//     set_mode(style);
-//   };
-
-//   useEffect(() => {
-//     if (orders?.data) {
-//       setTableData(orders.data);
-//     }
-//   }, [orders]);
-
-//   const handleDelete = (e, id, style) => {
-//     set_store_purchase_id(parseInt(id));
-//     set_mode_delete(style);
-//   };
-
-//   const navigate = useNavigate();
-//   // useEffect(() => {}, [data]);
-
-//   const handlePrint = (e, id, style) => {
-//     set_store_purchase_id(parseInt(id));
-//     set_print_mode(style);
-//   };
-
-//   const columns = useMemo(
-//     () => [
-//       {
-//         Header: "Sale Date",
-//         accessor: "sales_order_date",
-//         Cell: ({ value }) => <span>{moment(value).format("YYYY-MM-DD")}</span>,
-//       },
-//       {
-//         Header: "Sale Type",
-//         accessor: "sale_order_type",
-//       },
-//       {
-//         Header: "Order #",
-//         accessor: "sales_order_number",
-//       },
-//       {
-//         Header: "Total",
-//         accessor: "total",
-//       },
-//       {
-//         Header: "No Of Items",
-//         accessor: "pay_per_bale",
-//       },
-//       {
-//         Header: "Customer Name",
-//         accessor: "customer_name",
-//       },
-//       {
-//         Header: "Sale Person",
-//         accessor: "sales_person_number",
-//       },
-//       {
-//         Header: "Status",
-//         accessor: "status",
-//         Cell: ({ value }) => (
-//           <span style={{ color: getStatusColor(value) }}>{value}</span>
-//         ),
-//       },
-//       {
-//         Header: "Add",
-//         accessor: "add",
-//         Cell: ({ row }) => (
-//           <button
-//             onClick={(e) =>
-//               handleAdd(e, row.original.sales_order_number, "block")
-//             }
-//             disabled={row.original.status !== "New"}
-//           >
-//             <IoMdAdd />
-//           </button>
-//         ),
-//       },
-//       {
-//         Header: "Delete",
-//         accessor: "delete",
-//         Cell: ({ row }) => (
-//           <button
-//             onClick={(e) =>
-//               handleDelete(e, row.original.store_purchase_number, "block")
-//             }
-//             disabled={row.original.status !== "New"}
-//           >
-//             <MdDelete />
-//           </button>
-//         ),
-//       },
-//     ],
-//     [handleDelete, handleAdd]
-//   );
-
-//   // Function to determine status color
-//   const getStatusColor = (status) => {
-//     switch (status) {
-//       case "New":
-//         return "orange";
-//       case "In Transit":
-//         return "blue";
-//       case "Posted":
-//         return "green";
-//       default:
-//         return "inherit";
-//     }
-//   };
-
-//   if (isLoading) {
-//     return <Loader />;
-//   }
-//   return (
-//     <>
-//       <>
-//         <div style={{ display: `${mode}` }}>
-//           <AddOrderLines
-//             store_purchase_id={store_purchase_id}
-//             set_mode={set_mode}
-//           />
-//         </div>
-//         {/* <div style={{ display: `${mode_delete}` }}>
-//           <DeletePurchaseModal
-//             store_purchase_id={store_purchase_id}
-//             set_mode_delete={set_mode_delete}
-//           />
-//         </div> */}
-//         <div style={{ display: `${mode_print}` }}>
-//           <PrintSalesOrder
-//             purchase_header_id={store_purchase_id}
-//             set_mode_print={set_mode_print}
-//           />
-//         </div>
-//       </>
-//       <div>
-//         <p>*** All Sales Orders ***</p>
-
-//         <DataTable columns={columns} data={tableData} />
-//       </div>
-//     </>
-//   );
-// };
-// export default OrderHeaderList;
-
 import React, { useEffect, useMemo, useState } from "react";
 import Loader from "../../../components/Loader";
 import { useGetAllSalesOrderHeadersQuery } from "../../../slices/sales/salesOrderHeadersApiSlice";
@@ -181,21 +11,67 @@ import PrintSalesOrder from "./lines/PrintSalesOrder";
 import DataTable from "../../../components/general/DataTable";
 import moment from "moment";
 import "./order.css";
+import AddOrders from "./lines/AddOrders";
 
 const OrderHeaderList = () => {
+  // const { data: orders, isLoading } = useGetAllSalesOrderHeadersQuery();
+  // const [tableData, setTableData] = useState([]);
+
+  // const [isAddButtonActive, setIsAddButtonActive] = useState(false); // Step 1: State variable to track IoMdAdd button
+  // let timeDate = new TimeDate();
+  // const [mode_print, set_mode_print] = useState("none");
+  // const [mode, set_mode] = useState("none");
+  // const [mode_delete, set_mode_delete] = useState("none");
+  // const [store_purchase_id, set_store_purchase_id] = useState("");
+  // const [sales_order_number, set_sales_order_number] = useState("");
+  // const [sales_person_name, set_sales_person_name] = useState("");
+  // const [open, setOpen] = useState(false);
+
+  // const handleAdd = (e, id, style) => {
+  //   set_store_purchase_id(parseInt(id));
+  //   set_mode(style);
+  //   setIsAddButtonActive(true); // Step 2: Set IoMdAdd button active
+  // };
+
+  // useEffect(() => {
+  //   if (orders?.data) {
+  //     setTableData(orders.data);
+  //   }
+  // }, [orders]);
+
+  // const handleDelete = (e, id, style) => {
+  //   set_store_purchase_id(parseInt(id));
+  //   set_mode_delete(style);
+  // };
+  // const handleClickOpen = (e, id, name) => {
+  //   setOpen(true);
+  //   set_sales_order_number(parseInt(id));
+  //   set_sales_person_name(name);
+  // };
+
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
+  // const navigate = useNavigate();
+
+  let timeDate = new TimeDate();
   const { data: orders, isLoading } = useGetAllSalesOrderHeadersQuery();
   const [tableData, setTableData] = useState([]);
-  const [isAddButtonActive, setIsAddButtonActive] = useState(false); // Step 1: State variable to track IoMdAdd button
-  let timeDate = new TimeDate();
-  const [mode_print, set_mode_print] = useState("none");
-  const [mode, set_mode] = useState("none");
-  const [mode_delete, set_mode_delete] = useState("none");
-  const [store_purchase_id, set_store_purchase_id] = useState("");
+  const [sales_order_number, set_sales_order_number] = useState("");
+  const [sales_person_name, set_sales_person_name] = useState("");
 
-  const handleAdd = (e, id, style) => {
-    set_store_purchase_id(parseInt(id));
-    set_mode(style);
-    setIsAddButtonActive(true); // Step 2: Set IoMdAdd button active
+  const handleDelete = (e, id, style) => {
+    set_mode_delete(style);
+  };
+
+  const navigate = useNavigate();
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = (e, id, name) => {
+    setOpen(true);
+    set_sales_order_number(parseInt(id));
+    set_sales_person_name(name);
   };
 
   useEffect(() => {
@@ -203,13 +79,9 @@ const OrderHeaderList = () => {
       setTableData(orders.data);
     }
   }, [orders]);
-
-  const handleDelete = (e, id, style) => {
-    set_store_purchase_id(parseInt(id));
-    set_mode_delete(style);
+  const handleClose = () => {
+    setOpen(false);
   };
-
-  const navigate = useNavigate();
 
   const columns = useMemo(
     () => [
@@ -255,7 +127,12 @@ const OrderHeaderList = () => {
         Cell: ({ row }) => (
           <button
             onClick={(e) =>
-              handleAdd(e, row.original.sales_order_number, "block")
+              handleClickOpen(
+                e,
+                row.original.sales_order_number,
+                row.original.first_name,
+                "block"
+              )
             }
             disabled={row.original.status !== "New"}
           >
@@ -278,7 +155,7 @@ const OrderHeaderList = () => {
         ),
       },
     ],
-    [handleDelete, handleAdd]
+    [handleDelete]
   );
 
   // Function to determine status color...
@@ -302,7 +179,16 @@ const OrderHeaderList = () => {
   return (
     <>
       <>
-        <div style={{ display: `${mode}` }}>
+        <div>
+          <AddOrders
+            sales_person_name={sales_person_name}
+            sales_order_number={sales_order_number}
+            open={open}
+            handleClickOpen={handleClickOpen}
+            handleClose={handleClose}
+          />
+        </div>
+        {/* <div style={{ display: `${mode}` }}>
           <AddOrderLines
             store_purchase_id={store_purchase_id}
             set_mode={set_mode}
@@ -313,7 +199,7 @@ const OrderHeaderList = () => {
             purchase_header_id={store_purchase_id}
             set_mode_print={set_mode_print}
           />
-        </div>
+        </div> */}
       </>
       <div>
         <p>*** All Sales Orders ***</p>
