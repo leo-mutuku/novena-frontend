de6d8aeb5af6d46c05ba1354d780090f73e2637b
leo-mutuku <le.mutuku@gmail.com>
2024-09-13 17:54:37 +0300

    update

--- src/screens/ict/sidelinks.js
+++ src/screens/ict/sidelinks.js
@@ -2,16 +2,20 @@ export const sidelinks = [
   {
     menuTitle: "BioMetric",
     subMenus: [
-      { subMenuTitle: "Daily Attendance", to: "#", role: 6000 },
-      { subMenuTitle: "Allowed Absence", to: "#", role: 6000 },
+      { subMenuTitle: "Daily Attendance", to: "#", role: 9000 },
+      { subMenuTitle: "Attendance Report", to: "#", role: 9000 },
+      { subMenuTitle: "Allowed Absence", to: "#", role: 9000 },
+      { subMenuTitle: "Cancelled Attendance", to: "#", role: 9000 },
     ],
   },
 
   {
     menuTitle: "Bank Reconcialtion",
     subMenus: [
-      { subMenuTitle: "Period & Bank", to: "#", role: 6000 },
-      { subMenuTitle: "Upload Bank Statement", to: "#", role: 6000 },
+      { subMenuTitle: "Period & Bank", to: "#", role: 9000 },
+      { subMenuTitle: "Upload Bank Statement", to: "#", role: 9000 },
+      { subMenuTitle: "Compare & Match", to: "#", role: 9000 },
+      { subMenuTitle: "Pull Unreconciled", to: "#", role: 9000 },
     ],
   },
 
