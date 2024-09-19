ebcc8987100cfaed22bba723cab1a1196edcf929
leo-mutuku <le.mutuku@gmail.com>
2024-09-19 22:46:22 +0300

    update

--- src/screens/payment/sidelinks.js
+++ src/screens/payment/sidelinks.js
@@ -3,10 +3,11 @@ export const sidelinks = [
     menuTitle: "Payment",
     subMenus: [
       { subMenuTitle: "Pay Supplier", to: "purchases" },
-      { subMenuTitle: "Requisitions", to: "requsitions" },
+      { subMenuTitle: "Pay Vendor", to: "requsitions" },
       { subMenuTitle: "Payment Voucher", to: "paymentvoucher" },
       { subMenuTitle: "Salary Journal", to: "salaryjournal" },
       { subMenuTitle: "Supplier Report", to: "paymentreports" },
+      { subMenuTitle: "Vendor Report", to: "paymentreports" },
       { subMenuTitle: "Supplier Statement", to: "suppliersreport" },
     ],
   },
