50015f836d904e6ade5897cc06a98f960e0062a3
leo-mutuku <le.mutuku@gmail.com>
2024-09-20 10:20:12 +0300

    update

---
+++ src/slices/fleet/vendorApiSlice.js
@@ -0,0 +1,45 @@
+import { apiSlice } from "../apiSlice";
+const VENDORS_URL = "/api/v1/fleet/vehicles";
+
+export const vehicleApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    createVendor: builder.mutation({
+      query: (data) => ({
+        url: `${VENDORS_URL}/createvendor`,
+        method: "POST",
+        body: data,
+      }),
+      providesTags: ["Vendor"],
+    }),
+    getVendor: builder.query({
+      query: (id) => `${VENDORS_URL}/getvendor${id}`,
+      providesTags: ["Vendor"],
+    }),
+    getvendors: builder.query({
+      query: (data) => ({
+        url: `${VENDORS_URL}/getvendors`,
+        method: "GET",
+        body: data,
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: ["Vendor"],
+    }),
+    updatevendor: builder.mutation({
+      query: (data) => {
+        return {
+          url: `${VENDORS_URL}/updatevendor`,
+          method: "POST",
+          body: data,
+        };
+      },
+      invalidatesTags: ["Vendor"],
+    }),
+  }),
+});
+
+export const {
+  useCreateVendorMutation,
+  useGetVendorsQuery,
+  useUpdatevendorMutation,
+  useGetVendorQuery,
+} = vehicleApiSlice;
