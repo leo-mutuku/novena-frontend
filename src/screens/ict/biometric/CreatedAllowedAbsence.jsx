247a60bc96163b12d9c7dda629bfae7ceb6af72e
leo-mutuku <le.mutuku@gmail.com>
2024-09-21 08:35:44 +0300

    update

--- src/screens/ict/biometric/CreatedAllowedAbsence.jsx
+++ src/screens/ict/biometric/CreatedAllowedAbsence.jsx
@@ -1,7 +1,150 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+//import { useGetTodosQuery } from './apiSlice';
+import Loader from "../../../components/Loader";
+import { useGetAllStaffQuery } from "../../../slices/administration/staffApiSlice";
+import { Table, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaRegFileExcel } from "react-icons/fa6";
+import { CiEdit } from "react-icons/ci";
+import { MdDelete } from "react-icons/md";
+import { IoMdEye } from "react-icons/io";
+import { FaFilePdf, FaFileExcel } from "react-icons/fa";
+import DataTable from "../../../components/general/DataTable";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { baseUrlJasper } from "../../../slices/baseURLJasperReports";
+import { MdOutlineAlarmOn } from "react-icons/md";
 
 const CreatedAllowedAbsence = () => {
-  return <div>CreatedAllowedAbsence</div>;
-};
+  const { data: staffs, isLoading } = useGetAllStaffQuery();
+  const [tableData, setTableData] = useState([]);
+  const [loadingPdf, setLoadingPdf] = useState(false);
+  const [loadingExcel, setLoadingExcel] = useState(false);
 
+  useEffect(() => {
+    if (staffs?.data) {
+      setTableData(staffs.data);
+    }
+  }, [staffs]);
+
+  console.log(staffs?.data);
+
+  const handleDownloadPDF = async () => {
+    setLoadingPdf(true);
+    try {
+      await axios({
+        url: `${baseUrlJasper}/all/staffs/pdf`, // Endpoint on your Node.js server
+        method: "GET",
+        responseType: "blob", // Important: responseType 'blob' for binary data
+      }).then((response) => {
+        // Create a blob object from the binary data
+        const blob = new Blob([response.data], { type: "application/pdf" });
+
+        // Create a link element to trigger the download
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "all-staffs-report.pdf");
+        document.body.appendChild(link);
+
+        // Trigger the download
+        link.click();
+
+        // Clean up
+        window.URL.revokeObjectURL(url);
+      });
+    } catch (error) {
+      console.log(error.message);
+      // toast.error(error.message);
+    } finally {
+      setLoadingPdf(false);
+    }
+  };
+  const handleDownloadExcel = async () => {
+    setLoadingExcel(true);
+    try {
+      await axios({
+        url: `${baseUrlJasper}/all/staffs/excel`, // Endpoint on your Node.js server
+        method: "GET",
+        responseType: "blob", // Important: responseType 'blob' for binary data
+      }).then((response) => {
+        // Create a blob object from the binary data
+        const blob = new Blob([response.data], {
+          type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        });
+
+        // Create a link element to trigger the download
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "all-staffs-report.xlsx");
+        document.body.appendChild(link);
+
+        // Trigger the download
+        link.click();
+
+        // Clean up
+        window.URL.revokeObjectURL(url);
+      });
+    } catch (error) {
+      console.log(error.message);
+      // toast.error(error.message);
+    } finally {
+      setLoadingExcel(false);
+    }
+  };
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "#",
+        accessor: (row, index) => index + 1,
+      },
+      {
+        Header: "First Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last Name",
+        accessor: "last_name",
+      },
+      {
+        Header: "Contact",
+        accessor: "phone_number",
+      },
+
+      {
+        Header: "Category",
+        accessor: "category_name",
+      },
+
+      {
+        Header: "Mark In",
+        accessor: "edit",
+        Cell: ({ row }) => (
+          <Link to={`/administration/staff/update/${row.original.staff_id}`}>
+            <Button variant="outline-primary">
+              <MdOutlineAlarmOn />
+            </Button>
+          </Link>
+        ),
+      },
+    ],
+    []
+  );
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      <div>
+        <p>*** Allowed Absence ***</p>
+
+        <DataTable columns={columns} data={tableData} />
+      </div>
+    </>
+  );
+};
 export default CreatedAllowedAbsence;
