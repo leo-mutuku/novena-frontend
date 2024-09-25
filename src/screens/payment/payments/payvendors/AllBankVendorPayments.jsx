aaac667fd7c965ccccf245940b6d882b6dca1130
leo-mutuku <le.mutuku@gmail.com>
2024-09-25 12:06:39 +0300

    update

---
+++ src/screens/payment/payments/payvendors/AllBankVendorPayments.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllBankVendorPayments = () => {
+  return <div>AllBankVendorPayments</div>;
+};
+
+export default AllBankVendorPayments;
---
+++ src/screens/payment/payments/payvendors/AllCashVendorPayments.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllCashVendorPayments = () => {
+  return <div>AllCashVendorPayments</div>;
+};
+
+export default AllCashVendorPayments;
