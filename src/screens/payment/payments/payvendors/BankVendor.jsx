43f7ae9b2705145cba11543b1b3b06c82de89493
leo-mutuku <le.mutuku@gmail.com>
2024-09-25 03:52:17 +0300

    update

--- src/screens/payment/payments/payvendors/BankVendor.jsx
+++ src/screens/payment/payments/payvendors/BankVendor.jsx
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 
-import { useCreateBankSupplierPaymentMutation } from "../../../../slices/finance/bankAccountsApiSlice";
+import { useCreateBankVendorPaymentMutation } from "../../../../slices/finance/bankAccountsApiSlice";
 import { useGetAllBankAccountsQuery } from "../../../../slices/finance/bankAccountsApiSlice";
 import { useGetAllSuppliersQuery } from "../../../../slices/administration/suppliersApiSlice";
 import { useGetVendorsQuery } from "../../../../slices/fleet/vendorApiSlice";
 
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
 
 function BankVendors() {
+  const { userInfo } = useSelector((state) => state.auth);
   const [bank_id, set_bank_id] = useState("");
-  const [supplier_id, set_supplier_id] = useState("");
+  const [vendor_id, set_vendor_id] = useState("");
 
   const [amount, set_amount] = useState("");
   const [amount_in_words, set_amount_in_words] = useState("");
   const [created_by, set_created_by] = useState("");
 
   const [createSuplierBankPayment, { isLoading }] =
-    useCreateBankSupplierPaymentMutation();
+    useCreateBankVendorPaymentMutation();
   const { data: bankAccounts } = useGetAllBankAccountsQuery();
   const { data: suppliers } = useGetVendorsQuery();
 
@@ -32,17 +34,16 @@ function BankVendors() {
     try {
       const res = await createSuplierBankPayment({
         bank_id,
-        supplier_id,
+        vendor_id,
         amount_in_words,
         amount,
-
-        created_by,
+        created_by: userInfo?.first_name,
       }).unwrap();
       if (res.status == "failed") {
         toast.error(err?.data?.message || err.error);
       } else {
-        navigate("../allbanksupplierpayment");
-        toast.success("Store  Item created successfully");
+        navigate("../allbankvendorpayment");
+        toast.success("Payment created successfully");
       }
     } catch (err) {
       toast.error(err?.data?.message || err.error);
@@ -82,14 +83,14 @@ function BankVendors() {
             </Form.Group>
           </Col>
           <Col>
-            <Form.Group className="my-2" controlId="supplier_id">
+            <Form.Group className="my-2" controlId="vendor_id">
               <Form.Label>Vendor</Form.Label>
               <Form.Select
                 type="number"
                 required
-                placeholder="supplier_id"
-                value={supplier_id}
-                onChange={(e) => set_supplier_id(e.target.value)}
+                placeholder="vendor_id"
+                value={vendor_id}
+                onChange={(e) => set_vendor_id(e.target.value)}
               >
                 <option value={""}>Select vendor</option>
                 {suppliers?.data.map((item, index) => (
